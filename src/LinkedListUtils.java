7fa186fee530710629871be1f4997f632dc83e1f
jmanu863018 <manu.j.vv@gmail.com>
2019-05-08 20:35:51 -0400

    Implement insertSorted method

---
+++ src/LinkedListUtils.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+
+/*
+ * SD2x Homework #1
+ * Implement the methods below according to the specification in the assignment description.
+ * Please be sure not to change the signature of any of the methods!
+ */
+
+public class LinkedListUtils {
+
+    public static void insertSorted(LinkedList<Integer> list, int value) {
+        if (list != null) {
+            int position = 0;
+            for (int listValue : list) {
+                if (listValue >= value) {
+                    break;
+                }
+                position++;
+            }
+            list.add(position, value);
+        }
+    }
+
+
+    public static void removeMaximumValues(LinkedList<String> list, int N) {
+
+        /* IMPLEMENT THIS METHOD! */
+
+    }
+
+    public static boolean containsSubsequence(LinkedList<Integer> one, LinkedList<Integer> two) {
+
+        /* IMPLEMENT THIS METHOD! */
+
+        return true; // this line is here only so this code will compile if you don't modify it
+    }
+}
